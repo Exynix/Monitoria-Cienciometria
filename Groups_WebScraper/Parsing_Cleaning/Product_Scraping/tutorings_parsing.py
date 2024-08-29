27ceaeec3f10b7c9b33a0e00d273179c62a154a8
exynix <davids_barretom@javeriana.edu.co>
2024-08-29 11:49:04 -0500

    Upload of members parsing, as well as adding group name and GroupLAC to the table

--- Groups_WebScraper/Parsing_Cleaning/Product_Scraping/tutorings_parsing.py
+++ Groups_WebScraper/Parsing_Cleaning/Product_Scraping/tutorings_parsing.py
@@ -1,6 +1,8 @@
 #  ---------------------- Importación de librerias ---------------------- 
 from enum import Enum
 import sys
+import re 
+from typing import Optional
 
 import knime.scripting.io as knio
 
@@ -16,6 +18,81 @@ class MensajeVerficacion(Enum):
     ISSN_INVALIDO = -5
 
 #  ---------------------- Definición de funciones ---------------------- 
+
+# -------------------------------- Functions from the parsing utils library -------------------------------- 
+
+# Checks if a given HTML table has content beyond the header and columns.
+# Input: Complete HTML table to check. Number of "informative" rows (informative can be headers, column names. etc).
+def check_if_table_has_content(html_table: str, number_of_information_rows: int) -> int:
+    soup = BeautifulSoup(html_table, "html.parser")
+
+    total_number_of_rows = len(soup.find_all("tr"))
+
+    if (total_number_of_rows <= number_of_information_rows):
+
+        # If the total number of rows is equal to the informative rows, then the table is empty.
+        if (total_number_of_rows == number_of_information_rows):
+            return MensajeVerficacion.TABLA_SIN_CONTENIDOS.value
+        # In any other case, the table is invalid.
+        else:
+            return MensajeVerficacion.TABLA_INVALIDA.value
+
+    # A number of total rows bigger than the num. of informative rows means that there's at least one product. 
+    if (total_number_of_rows > number_of_information_rows):
+        return MensajeVerficacion.TABLA_VALIDA.value
+
+
+# -------------
+
+# Verificacion de existencia de tag <img>. Dice si el producto académico fue avalado.
+def revisar_producto_avalado(fila_html: str) -> str:
+     
+    if (fila_html.td.img != None):
+        return "Si"
+    else:
+        return "No"
+
+# ------------
+# ------------
+def check_if_string_is_alphanumeric (input_string) -> bool:
+
+    alphanumeric_regex = "[A-Za-z0-9]+"
+    search_result = re.search(alphanumeric_regex, input_string) 
+    
+    if search_result == None:
+        return False;
+    else:
+        return True;
+
+# ------------
+
+def revisar_string_vacio(input_string: str) -> str:
+    if(len(input_string) == 0):
+        return None
+    else:
+        return input_string
+
+# ------------
+def match_and_verify_regex_expression (string_to_check: str, regex_pattern: str) -> Optional[str]:
+
+    # Regex mathching.
+    search_result = re.search (regex_pattern, string_to_check) 
+
+    # Verification of match existance.
+    if search_result == None:
+        # print("No match found.")
+        # print(" - Pattern: ", regex_pattern)
+        # print(" - String being searched: ", repr(string_to_check))
+        return None
+    
+   # If the function returns false, then the string doesn't have any digits or numbers. That's why we return null.
+    if check_if_string_is_alphanumeric (search_result.group(0)) == False:
+        print("string sent is whitespace or doesn't contain alphanumeric characters.", repr(string_to_check))
+        return None
+    else:
+        return search_result.group(0).strip()
+ 
+# ------------ Functions specific to parsing of tutorings. -----------------
 
 # Revisa si la tabla dada tiene registros, más alla del titulo y columnas de la tabla.
 # Input: Tabla HTML completa de articulos.
@@ -91,30 +168,6 @@ def definir_estado(fecha_finalizacion: str) -> int:
         return "En Curso"
     else:
         return "Finalizado"
-
-# ------
-
-# Deje el nombre de las variables de estudiantes así a proposito para alegrar un poco el code review jajajaja.
-def extraer_limpiar_nombres_estudiantes(linea_completa_estudiantes_programa):
-    
-    indice_nombres = linea_completa_estudiantes_programa.find("Nombre del estudiante:")
-    indice_separador_programa = linea_completa_estudiantes_programa.find("Programa académico:")
-    indice_coma = indice_separador_programa - 2
-
-    offset_nombres = len("Nombre del estudiante:")
-
-    substr_estudiantes = linea_completa_estudiantes_programa[indice_nombres : indice_coma]
-    #print(repr(substr_estudiantes))
-
-    substr_estudiantes = substr_estudiantes.strip()
-    #print(repr(substr_estudiantes))
-
-    estudiantes_sucios = substr_estudiantes [offset_nombres : ]
-
-    estudiantes_limpios = estudiantes_sucios.strip()
-
-    return revisar_string_vacio(estudiantes_limpios)
-    
 
 # ------
 
@@ -220,7 +273,9 @@ for index, tabla in enumerate(tablas_html):
         estado = definir_estado(fecha_finalizacion)
 
         linea_completa_estudiantes_programa = tags_br[1].next_sibling
-        nombres_estudiantes = extraer_limpiar_nombres_estudiantes(linea_completa_estudiantes_programa)
+        linea_completa_estudiantes_programa = linea_completa_estudiantes_programa.replace("\n", '')
+        linea_completa_estudiantes_programa = linea_completa_estudiantes_programa.rstrip()
+        nombres_estudiantes = match_and_verify_regex_expression(linea_completa_estudiantes_programa, "(?<=Nombre del estudiante:).*?(?=(,|Programa académico))")
 
         programa_academico = extraer_limpiar_programa_academico(linea_completa_estudiantes_programa)
 
