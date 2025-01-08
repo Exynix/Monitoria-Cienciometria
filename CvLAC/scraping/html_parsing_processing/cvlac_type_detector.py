50fa466772759c780e9d79eae7cf08c4116f7670
David <davids_barretom@javeriana.edu.co>
2025-01-08 00:50:32 -0500

    First finished implementation of cvlac type detector.

--- CvLAC/scraping/html_parsing_processing/cvlac_type_detector.py
+++ CvLAC/scraping/html_parsing_processing/cvlac_type_detector.py
@@ -34,21 +34,27 @@ def parse_and_identify_page_type(html_string: str) -> str:
         return CvlacType.PRIVATE.value
 
     # 2. Identification of empty pages. Second simplest.
-    content_div = soup.select("div.container") # First level. body > div
-    main_content_tbody = content_div.find("tbody")
+    content_div = soup.select_one("div.container") # First level. body > div
+    main_content_tbody = content_div.find("tbody") #  Third level. div > table > tbody
+    social_media_tr = main_content_tbody.find_all("tr")[2] # Fourth level. div > table > tbody > tr
+    social_media_td = social_media_tr.find("td") # Fifth level. div > table > tbody > tr > td
+    social_media_html_tags = [element for element in social_media_td.contents if element.name is not None]
+
+    # If there's only 1 element inside the social media <td>, the profile is counted as empty, as there's always
+    # as a minimum, an <a> tag inside.
+    if len(social_media_html_tags) == 1:
+        return CvlacType.EMPTY.value
+
+    # 3. If inside the social media <td>, there's 2 tags (<a> and <table>) the profile is likely filled, or full.
+    # The profile is counted as a normal profile.
+    if len(social_media_html_tags) == 2:
+        return CvlacType.NORMAL.value
+
+    # 4. If none of the if statements were entered, this is an unknown page.
+    return CvlacType.UNKNOWN.value
+
 # ----------------------------------------------------------
 
 # ------------------ "Main" -------------------------------------------
 all_cvlacs_df = knio.input_tables[0].to_pandas()
 all_cvlacs_df["Type"] = all_cvlacs_df["Document"].apply(parse_and_identify_page_type)
-
-
-
-
-
-normal_cvlacs_list: list[ParsedCvlac]
-private_cvlacs_df: list[ParsedCvlac]
-empty_cvlacs_df: list[ParsedCvlac]
-unknown_type_cvlacs: list[ParsedCvlac]
-
-knio.output_tables[0] = knio.input_tables[0]
