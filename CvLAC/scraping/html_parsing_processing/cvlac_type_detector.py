36e8cab4749d2b45617f702cba24166946aaf6c9
David <davids_barretom@javeriana.edu.co>
2025-01-08 22:31:56 -0500

    Refactored python script logic with implementation changes made in testing.

--- CvLAC/scraping/html_parsing_processing/cvlac_type_detector.py
+++ CvLAC/scraping/html_parsing_processing/cvlac_type_detector.py
@@ -27,38 +27,53 @@ class ParsedCvlac(NamedTuple):
     cvlac_type: CvlacType
 
 # ----------------------------------------------------------
-# Definition of detectiion function
+
+
+def detect_private_pages(bs4Soup) -> str:
+    private_msg_baseline = "La información de este currículo no está disponible por solicitud del investigador"
+    private_msg_scraped = bs4Soup.find_all("blockquote")[1].text.strip()
+    if private_msg_scraped == private_msg_baseline:
+        return CvlacType.PRIVATE.value
+    print("Error in parsing private page.")
+    print(traceback.format_exc())
+    return CvlacType.ERROR.value
+
+
+# Identification of empty and normal pages. The detection for both goes hand in hand.
+def detect_private_and_normal_pages(bs4Soup) -> str:
+    # Firstly, the image element in the green message is searched. It serves as the initial reference point.
+    green_msg_tick_img = bs4Soup.find("img", {"height": "15px", "width": "15px"})
+    # Now, we go back to it's parent <td>. We have to go 3 levels up. img < blockquote < td <td
+    green_msg_parent_td = green_msg_tick_img.parent.parent.parent
+    green_msg_parent_td_siblings = [element for element in green_msg_parent_td.next_siblings if element.name is not None]
+
+    # Now that we are on the same level as all other <tr>s (which then contain the products themselves). We iterate over all next_siblings.
+    is_table_empty: bool = True
+    for sibling_tr in green_msg_parent_td_siblings:
+        # Inside each sibling tr, we look if the child <td> has more than 1 child. If it DOES, then it's not empty.
+        # If it has 1 or 0 childs, the page so far is empty.
+        sibling_tr_children_list = [element for element in sibling_tr.td.children if element.name is not None]
+        if len(sibling_tr_children_list) > 1:
+            is_table_empty = False
+            return CvlacType.NORMAL.value
+    return CvlacType.EMPTY.value
+
+
+
+# Definition of main detection function
 def parse_and_identify_page_type(df_row) -> str:
 
     html_string = df_row["Document"]
     cvlac_url = df_row["url_cvlac"]
 
     soup = BeautifulSoup(html_string, "html.parser")
-
     try:
-        # 1. Identification of private pages. They are the simplest ones.
-        private_msg_baseline = "La información de este currículo no está disponible por solicitud del investigador"
-        private_msg_scraped = soup.find_all("blockquote")[1].text.strip()
-        if private_msg_scraped == private_msg_baseline:
-            return CvlacType.PRIVATE.value
-
-        # 2. Identification of empty pages. Second simplest.
-        academic_formation_string = "Formación Académica"
-        academic_formation_element_string = soup.find(string=academic_formation_string)
-
-        if academic_formation_element_string is None:
-            return CvlacType.EMPTY.value
-
-        if academic_formation_element_string is not None:
-            return CvlacType.NORMAL.value
-
+        return detect_private_and_normal_pages(soup)
 
         # 4. If none of the if statements were entered, this is an unknown page.
         return CvlacType.UNKNOWN.value
     except:
-        print(f"Error ocurred in processing of cvlac: {cvlac_url}")
-        print(traceback.format_exc())
-        return CvlacType.ERROR.value
+        return detect_private_pages(soup)
 
 
 # ----------------------------------------------------------
